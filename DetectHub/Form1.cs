b6c2e2afb6703a9d9c5a3405df513737f608bebf
t0ki Mier <t0kis@outlook.com>
2023-09-25 16:02:03 +0300

    ui custumize

--- DetectHub/Form1.cs
+++ DetectHub/Form1.cs
@@ -1,7 +1,8 @@
-﻿using OpenCvSharp;
-using OpenCvSharp.Extensions;
-using Compunet.YoloV8;
-using System.Diagnostics;
+﻿    using OpenCvSharp;
+    using OpenCvSharp.Extensions;
+    using Compunet.YoloV8;
+    using System.Diagnostics;
+using System.Windows.Forms;
 
 
 namespace DetectHub
@@ -74,7 +75,7 @@ namespace DetectHub
                 {
                     MessageBox.Show("Указанна неверная .onnx модель.", "Ошибка загрузки");
                     model_path = null;
-                    name_model_label.Text = "";
+                    name_model_label.Text = "Модель не загружена";
                 }
             }
         }
@@ -154,7 +155,7 @@ namespace DetectHub
             //capture.FrameWidth = frame_width;
 
             this.Text = "DetectHub - Распознование объектов";
-            this.Icon = new System.Drawing.Icon("..\\..\\..\\assets\\icon.ico");
+            this.Icon = new Icon("..\\..\\..\\assets\\icon.ico");
             this.Size = new System.Drawing.Size(width_window, height_window);
             this.BackColor = Color.Gray;
             this.StartPosition = FormStartPosition.CenterScreen;
@@ -167,15 +168,6 @@ namespace DetectHub
             image_output.Width = 400 + 240;
             image_output.Height = 400 + 80;
             this.Controls.Add(image_output);
-
-            PictureBox first_element = new();
-            first_element.Image = Bitmap.FromFile("..\\..\\..\\assets\\first_element.png");
-            first_element.Left = 15;
-            first_element.Top = 150;
-            first_element.Width = 450;
-            first_element.Height = 300;
-            first_element.BackgroundImageLayout = ImageLayout.Stretch;
-            //this.Controls.Add(first_element);
 
             button_start_stop.Location = new System.Drawing.Point(740, 630);
             button_start_stop.FlatStyle = FlatStyle.Flat;
@@ -195,19 +187,22 @@ namespace DetectHub
 
             Label open_label = new();
             open_label.Text = "Загрузить .onnx модель";
-            open_label.Location = new System.Drawing.Point(20, 150);
+            open_label.Location = new System.Drawing.Point(40, 150);
             open_label.Font = new Font("Arial", 12);
             open_label.ForeColor = Color.White;
             open_label.Width = 280;
             open_label.Height = 40;
+            open_label.BackColor = ColorTranslator.FromHtml("#7c5bc2");
             this.Controls.Add(open_label);
 
 
-            name_model_label.Location = new System.Drawing.Point(20, 190);
+            name_model_label.Location = new System.Drawing.Point(40, 190);
+            name_model_label.Text = "Модель не загружена";
             name_model_label.Font = new Font("Arial", 11);
             name_model_label.ForeColor = Color.White;
             name_model_label.Width = 280;
             name_model_label.Height = 40;
+            name_model_label.BackColor = ColorTranslator.FromHtml("#7c5bc2");
             this.Controls.Add(name_model_label);
 
             button_model_open.Location = new System.Drawing.Point(320, 150);
@@ -224,9 +219,10 @@ namespace DetectHub
             button_model_open.MouseUp += button_model_open_MouseUp;
             button_model_open.BackgroundImage = Image.FromFile("..\\..\\..\\assets\\open.png");
             button_model_open.BackgroundImageLayout = ImageLayout.Stretch;
+            button_model_open.BackColor = ColorTranslator.FromHtml("#7c5bc2");
             this.Controls.Add(button_model_open);
 
-            button_screenshot.Location = new System.Drawing.Point(20, 340);
+            button_screenshot.Location = new System.Drawing.Point(40, 340);
             button_screenshot.FlatStyle = FlatStyle.Flat;
             button_screenshot.FlatAppearance.BorderSize = 0;
             button_screenshot.FlatAppearance.MouseOverBackColor = Color.Transparent;
@@ -240,20 +236,22 @@ namespace DetectHub
             button_screenshot.MouseUp += button_screenshot_MouseUp;
             button_screenshot.BackgroundImage = Image.FromFile("..\\..\\..\\assets\\screenshot.png");
             button_screenshot.BackgroundImageLayout = ImageLayout.Stretch;
+            button_screenshot.BackColor = ColorTranslator.FromHtml("#7c5bc2");
             this.Controls.Add(button_screenshot);
 
 
             Label taskbar_label = new();
             taskbar_label.Text = "Confidence";
-            taskbar_label.Location = new System.Drawing.Point(20, 240);
+            taskbar_label.Location = new System.Drawing.Point(40, 240);
             taskbar_label.Font = new Font("Arial", 12);
             taskbar_label.ForeColor = Color.White;
             taskbar_label.Width = 150;
             taskbar_label.Height = 40;
+            taskbar_label.BackColor = ColorTranslator.FromHtml("#7c5bc2");
             this.Controls.Add(taskbar_label);
 
             fps_counter.Text = "FPS: ";
-            fps_counter.Location = new System.Drawing.Point(20, 440);
+            fps_counter.Location = new System.Drawing.Point(40, 440);
             fps_counter.Font = new Font("Arial", 12);
             fps_counter.ForeColor = Color.White;
             fps_counter.Width = 150;
@@ -261,7 +259,7 @@ namespace DetectHub
             this.Controls.Add(fps_counter);
 
             cycle_time.Text = "Время цикла: ";
-            cycle_time.Location = new System.Drawing.Point(20, 480);
+            cycle_time.Location = new System.Drawing.Point(40, 480);
             cycle_time.Font = new Font("Arial", 12);
             cycle_time.ForeColor = Color.White;
             cycle_time.Width = 350;
@@ -269,7 +267,7 @@ namespace DetectHub
             this.Controls.Add(cycle_time);
 
             object_counter.Text = "Объектов: ?";
-            object_counter.Location = new System.Drawing.Point(20, 520);
+            object_counter.Location = new System.Drawing.Point(40, 520);
             object_counter.Font = new Font("Arial", 12);
             object_counter.ForeColor = Color.White;
             object_counter.Width = 350;
@@ -278,11 +276,12 @@ namespace DetectHub
 
 
             confidence_label.Text = "0.35";
-            confidence_label.Location = new System.Drawing.Point(20, 280);
+            confidence_label.Location = new System.Drawing.Point(40, 280);
             confidence_label.Font = new Font("Arial", 11);
             confidence_label.ForeColor = Color.White;
             confidence_label.Width = 60;
             confidence_label.Height = 40;
+            confidence_label.BackColor = ColorTranslator.FromHtml("#7c5bc2");
             this.Controls.Add(confidence_label);
 
 
@@ -293,8 +292,17 @@ namespace DetectHub
             trackBar1.SmallChange = 1;
             trackBar1.Value = 35;
             trackBar1.Location = new System.Drawing.Point(220, 240);
+            trackBar1.BackColor = ColorTranslator.FromHtml("#7c5bc2"); trackBar1.ForeColor = Color.White;
             trackBar1.Width = 200;
             this.Controls.Add(trackBar1);
+            PictureBox first_element = new();
+            first_element.Image = Bitmap.FromFile("..\\..\\..\\assets\\first_element.png");
+            first_element.Left = 14;
+            first_element.Top = 110;
+            first_element.Width = 450;
+            first_element.Height = 300;
+            first_element.BackgroundImageLayout = ImageLayout.Stretch;
+            this.Controls.Add(first_element);
 
             openFileDialog1.Filter = "ONNX Files (*.onnx)|*.onnx";
 
@@ -311,108 +319,108 @@ namespace DetectHub
             Process.GetCurrentProcess().PriorityClass = ProcessPriorityClass.High;
         }
 
-        private void Button_start_stop_MouseEnter(object? sender, EventArgs e)
-        {
-            throw new NotImplementedException();
-        }
-
-        void plot_one_box(int[] x, Mat img, string label = null, int line_thickness = 2)
-        {
-            int tl = line_thickness == 0 ? (int)(.002 * (img.Height + img.Width) / 2 + 1) : line_thickness;
-
-            OpenCvSharp.Point c1 = new(x[0], x[1]);
-            OpenCvSharp.Point c2 = new(x[2], x[3]);
-            Cv2.Rectangle(img, c1, c2, Scalar.Orange, tl, LineTypes.AntiAlias);
-            if (label != null)
+            private void Button_start_stop_MouseEnter(object? sender, EventArgs e)
             {
-                int tf = Math.Max(tl - 1, 1);
-                c2 = new OpenCvSharp.Point(c1.X + label.Length * 10.8, c1.Y - 18);
-                Cv2.Rectangle(img, new OpenCvSharp.Point(c1.X - 1, c1.Y), c2, Scalar.Orange, -1, LineTypes.AntiAlias);
-                Cv2.PutText(img, label, new OpenCvSharp.Point(c1.X + 2, c1.Y - 4), HersheyFonts.HersheyDuplex, .6, new Scalar(225, 255, 255), tf, LineTypes.AntiAlias);
+                throw new NotImplementedException();
             }
-        }
-        Mat ProcessFrame(Mat img)
-        {
-            Bitmap bitmap = img.ToBitmap();
-            using (MemoryStream ms = new())
-            {
-                bitmap.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);
-                data = ms.ToArray();
-            }
-            var result = predictor.Detect(data);
 
-            objects_on_frame = 0;
-            foreach (var box in result.Boxes)
+            void plot_one_box(int[] x, Mat img, string label = null, int line_thickness = 2)
             {
-                objects_on_frame++;
-                int[] x = { box.Bounds.X, box.Bounds.Y, box.Bounds.X + box.Bounds.Width, box.Bounds.Y + box.Bounds.Height };
-                plot_one_box(x, img, $"{box.Class.Name}: {Math.Round(box.Confidence, 2)}");
-            }
-            object_counter.Text = $"Объектов: {objects_on_frame}";
-            return img;
-        }
-        private void ListWebcams()
-        {
-            DirectShowLib.DsDevice[] devices = DirectShowLib.DsDevice.GetDevicesOfCat(DirectShowLib.FilterCategory.VideoInputDevice);
-            foreach (DirectShowLib.DsDevice device in devices)
-            {
-                webcam_combo.Items.Add(device.Name);
-            }
-            webcam_combo.SelectedIndex = 0;
-        }
+                int tl = line_thickness == 0 ? (int)(.002 * (img.Height + img.Width) / 2 + 1) : line_thickness;
 
-        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            capture.Release();
-            capture = new VideoCapture(webcam_combo.SelectedIndex);
-        }
-        private void CaptureFrame(object sender, EventArgs e)
-        {
-            ms_counter++;
-            Stopwatch stopwatch = new();
-            stopwatch.Start();
-            Mat frame = capture.RetrieveMat();
-            if (frame != null)
-            {
-                if (button_start_stop_counter % 2 == 1)
+                OpenCvSharp.Point c1 = new(x[0], x[1]);
+                OpenCvSharp.Point c2 = new(x[2], x[3]);
+                Cv2.Rectangle(img, c1, c2, Scalar.Orange, tl, LineTypes.AntiAlias);
+                if (label != null)
                 {
-                    confidence = (double)trackBar1.Value / trackBar1.Maximum;
-                    confidence_label.Text = $"{confidence}";
-                    if (predictor != null)
+                    int tf = Math.Max(tl - 1, 1);
+                    c2 = new OpenCvSharp.Point(c1.X + label.Length * 10.8, c1.Y - 18);
+                    Cv2.Rectangle(img, new OpenCvSharp.Point(c1.X - 1, c1.Y), c2, Scalar.Orange, -1, LineTypes.AntiAlias);
+                    Cv2.PutText(img, label, new OpenCvSharp.Point(c1.X + 2, c1.Y - 4), HersheyFonts.HersheyDuplex, .6, new Scalar(225, 255, 255), tf, LineTypes.AntiAlias);
+                }
+            }
+            Mat ProcessFrame(Mat img)
+            {
+                Bitmap bitmap = img.ToBitmap();
+                using (MemoryStream ms = new())
+                {
+                    bitmap.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);
+                    data = ms.ToArray();
+                }
+                var result = predictor.Detect(data);
+
+                objects_on_frame = 0;
+                foreach (var box in result.Boxes)
+                {
+                    objects_on_frame++;
+                    int[] x = { box.Bounds.X, box.Bounds.Y, box.Bounds.X + box.Bounds.Width, box.Bounds.Y + box.Bounds.Height };
+                    plot_one_box(x, img, $"{box.Class.Name}: {Math.Round(box.Confidence, 2)}");
+                }
+                object_counter.Text = $"Объектов: {objects_on_frame}";
+                return img;
+            }
+            private void ListWebcams()
+            {
+                DirectShowLib.DsDevice[] devices = DirectShowLib.DsDevice.GetDevicesOfCat(DirectShowLib.FilterCategory.VideoInputDevice);
+                foreach (DirectShowLib.DsDevice device in devices)
+                {
+                    webcam_combo.Items.Add(device.Name);
+                }
+                webcam_combo.SelectedIndex = 0;
+            }
+
+            private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+            {
+                capture.Release();
+                capture = new VideoCapture(webcam_combo.SelectedIndex);
+            }
+            private void CaptureFrame(object sender, EventArgs e)
+            {
+                ms_counter++;
+                Stopwatch stopwatch = new();
+                stopwatch.Start();
+                Mat frame = capture.RetrieveMat();
+                if (frame != null)
+                {
+                    if (button_start_stop_counter % 2 == 1)
                     {
-                        predictor.Parameters.Confidence = (float)confidence;
+                        confidence = (double)trackBar1.Value / trackBar1.Maximum;
+                        confidence_label.Text = $"{confidence}";
+                        if (predictor != null)
+                        {
+                            predictor.Parameters.Confidence = (float)confidence;
+                        }
                     }
+                    if (button_start_stop_counter % 2 == 0)
+                    {
+                        Mat plotted_img = ProcessFrame(frame);
+                        outimg_bitmap = BitmapConverter.ToBitmap(plotted_img);
+                        image_output.Image = outimg_bitmap;
+                    }
+                    else
+                    {
+                        object_counter.Text = "Объектов: ?";
+                        outimg_bitmap = BitmapConverter.ToBitmap(frame);
+                        image_output.Image = outimg_bitmap;
+                    }
+                    GC.Collect();
                 }
-                if (button_start_stop_counter % 2 == 0)
+                stopwatch.Stop();
+                ms_cycles[ms_counter] = stopwatch.ElapsedMilliseconds;
+                if (ms_counter == 9)
                 {
-                    Mat plotted_img = ProcessFrame(frame);
-                    outimg_bitmap = BitmapConverter.ToBitmap(plotted_img);
-                    image_output.Image = outimg_bitmap;
+                    fps_counter.Text = $"FPS: {Convert.ToString(1000 / (ms_cycles.Sum() / 10))}";
+                    cycle_time.Text = $"Время цикла: {ms_cycles.Sum() / 10} ms";
+                    ms_counter = 0;
                 }
-                else
-                {
-                    object_counter.Text = "Объектов: ?";
-                    outimg_bitmap = BitmapConverter.ToBitmap(frame);
-                    image_output.Image = outimg_bitmap;
-                }
-                GC.Collect();
             }
-            stopwatch.Stop();
-            ms_cycles[ms_counter] = stopwatch.ElapsedMilliseconds;
-            if (ms_counter == 9)
-            {
-                fps_counter.Text = $"FPS: {Convert.ToString(1000 / (ms_cycles.Sum() / 10))}";
-                cycle_time.Text = $"Время цикла: {ms_cycles.Sum() / 10} ms";
-                ms_counter = 0;
-            }
-        }
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            //AllocConsole();
+            private void Form1_Load(object sender, EventArgs e)
+            {
+                //AllocConsole();
+            }
+            //[dllimport("kernel32.dll", setlasterror = true)]
+            //[return: marshalas(unmanagedtype.bool)]
+            //static extern bool allocconsole();
         }
-        //[dllimport("kernel32.dll", setlasterror = true)]
-        //[return: marshalas(unmanagedtype.bool)]
-        //static extern bool allocconsole();
     }
-}
